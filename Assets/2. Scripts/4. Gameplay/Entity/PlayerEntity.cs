19b7ea0b7926466ce7c0742eeb799d5768758579
HKgoat <baekhoymamber@gmail.com>
2026-01-06 21:04:50 +0900

    Create VFX Effect Color  , Foloder ReFactoring

---
+++ Assets/2. Scripts/4. Gameplay/Entity/PlayerEntity.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class PlayerEntity : MonoBehaviour
+{
+    [Header("Base Stats (ONLY HERE)")]
+    [SerializeField] private float _baseMoveSpeed = 12f;
+
+    private RunScope _scope;
+
+    public PlayerController Controller { get; private set; }
+    public AbilitySystem Ability { get; private set; }
+    public PlayerMeleeAutoAttack Melee { get; private set; }
+
+    public void Construct(RunScope scope)
+    {
+        _scope = scope;
+
+        Controller = GetOrAdd<PlayerController>();
+        Controller.SetBaseMoveSpeed(_baseMoveSpeed); 
+
+        Ability = GetOrAdd<AbilitySystem>();
+        Ability.Construct(_scope);
+
+        Melee = GetOrAdd<PlayerMeleeAutoAttack>();
+        Melee.Construct(_scope);
+    }
+    public void SetMoveSpeedMultiplier(float mul) => Controller?.SetMoveSpeedMultiplier(mul);
+    public void AddMoveSpeedMultiplier(float add) => Controller?.AddMoveSpeedMultiplier(add);
+
+    private T GetOrAdd<T>() where T : Component
+    {
+        var c = GetComponent<T>();
+        if (c == null) c = gameObject.AddComponent<T>();
+        return c;
+    }
+}
